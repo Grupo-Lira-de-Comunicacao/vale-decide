8605b100ca04af0fc0fd41d2938ed1f64a441f91
Sérgio Lira <83613556+SPLIRA@users.noreply.github.com>
2026-08-23 19:28:51 -0300

    fix: corrigir JSX do painel admin

--- src/AdminApp.jsx
+++ src/AdminApp.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { adminApi as defaultApi } from './services/adminApi.js'
 
 const ADMIN_EMAIL = 'splira@gmail.com'
