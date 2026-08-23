c19ea63bd6fd618b74fb9da0cf4e987ef6ae886c
Sérgio Lira <83613556+SPLIRA@users.noreply.github.com>
2026-08-23 19:26:55 -0300

    style: carregar estilos do painel admin

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import AdminApp from './AdminApp.jsx'
 import './App.css'
+import './Admin.css'
 
 const Root = window.location.pathname.startsWith('/admin') ? AdminApp : App
 
